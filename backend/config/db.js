2cfe2efe4b641a9b67ad08aa6ea88dfbc1214651
Zaifa25 <mhuzaifamumtaz77@gmail.com>
2026-07-29 16:11:30 +0500

    docs(backend): add JSDoc documentation to db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,15 @@
+/**
+ * @file db.js
+ * @description Initializes and exports single instance of Prisma Client ORM connection.
+ */
+
 const { PrismaClient } = require('@prisma/client');
 
+/**
+ * Global Prisma Client instance for database query execution.
+ * @type {PrismaClient}
+ */
 const prisma = new PrismaClient();
 
 module.exports = prisma;
+
